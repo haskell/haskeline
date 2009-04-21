8aebe828c0ee19273c3f528bb0efe0535bd37461
judah.jacobson <judah.jacobson@gmail.com>
2009-04-21 02:09:48 +0000

    Update Setup script to pull -I and -L dirs from the package dependencies.  (e.g. the rts package can point to /usr/local/lib on OpenBSD.)

--- Setup.hs
+++ Setup.hs
@@ -7,6 +7,9 @@ import Distribution.Simple.Setup
 import Distribution.Simple.LocalBuildInfo
 import Distribution.Simple.Utils
 
+import Distribution.Simple.PackageIndex
+import qualified Distribution.InstalledPackageInfo as Installed
+
 import System.IO
 import System.Exit
 import System.Directory
@@ -24,8 +27,7 @@ myHooks
             postConf = \args flags pkgDescr lbi -> do
                             let Just lib = library pkgDescr
                             let bi = libBuildInfo lib
-                            bi' <- maybeSetLibiconv flags bi
-                                                        (withPrograms lbi)
+                            bi' <- maybeSetLibiconv flags bi lbi
                             let pkgDescr' = pkgDescr {
                                                 library = Just lib {
                                                     libBuildInfo = bi'}}
@@ -33,31 +35,31 @@ myHooks
             }
 
 -- Test whether compiling a c program that links against libiconv needs -liconv.
-maybeSetLibiconv :: ConfigFlags -> BuildInfo -> ProgramConfiguration -> IO BuildInfo
-maybeSetLibiconv flags bi progConf = do
+maybeSetLibiconv :: ConfigFlags -> BuildInfo -> LocalBuildInfo -> IO BuildInfo
+maybeSetLibiconv flags bi lbi = do
+    let biWithIconv = addIconv bi
     let verb = fromFlag (configVerbosity flags)
     if hasFlagSet flags (FlagName "libiconv")
         then do
             putStrLn "Using -liconv."
             writeBuildInfo "iconv"
-            return bi
+            return biWithIconv
         else do
     putStr "checking whether to use -liconv... "
     hFlush stdout
-    worksWithout <- tryCompile iconv_prog bi progConf verb
+    worksWithout <- tryCompile iconv_prog bi lbi verb
     if worksWithout
         then do
             putStrLn "not needed."
             writeBuildInfo ""
             return bi
         else do
-    let newBI = addIconv bi
-    worksWith <- tryCompile iconv_prog newBI progConf verb
+    worksWith <- tryCompile iconv_prog biWithIconv lbi verb
     if worksWith
         then do
             putStrLn "using -liconv."
             writeBuildInfo "iconv"
-            return newBI
+            return biWithIconv
         else error "Unable to link against the iconv library."
   where
     -- Cabal (at least 1.6.0.1) won't parse an empty buildinfo file.
@@ -67,27 +69,31 @@ maybeSetLibiconv flags bi progConf = do
 hasFlagSet :: ConfigFlags -> FlagName -> Bool
 hasFlagSet cflags flag = Just True == lookup flag (configConfigurationsFlags cflags)
 
-tryCompile :: String -> BuildInfo -> ProgramConfiguration -> Verbosity -> IO Bool
-tryCompile program bi progConf verb = handle processExit $ handle processException $ do
+tryCompile :: String -> BuildInfo -> LocalBuildInfo -> Verbosity -> IO Bool
+tryCompile program bi lbi verb = handle processExit $ handle processException $ do
     tempDir <- getTemporaryDirectory
     withTempFile tempDir ".c" $ \fname h -> do
         hPutStr h program
         hClose h
         -- TODO take verbosity from the args.
-        rawSystemProgramStdoutConf verb gccProgram progConf (fname : args)
+        rawSystemProgramStdoutConf verb gccProgram (withPrograms lbi) (fname : args)
         return True
   where
     processException :: IOException -> IO Bool
     processException e = return False
     processExit = return . (==ExitSuccess)
-    args = concat [ []
-                  , ccOptions bi
+    -- Mimicing Distribution.Simple.Configure
+    deps = topologicalOrder (installedPkgs lbi)
+    args = concat
+                  [ ccOptions bi
                   , cppOptions bi
                   , ldOptions bi
                   -- --extra-include-dirs and --extra-lib-dirs are included
                   -- in the below fields.
-                  , map ("-I" ++) (includeDirs bi)
-                  , map ("-L" ++) (extraLibDirs bi)
+                  -- Also sometimes a dependency like rts points to a nonstandard
+                  -- include/lib directory where iconv can be found. 
+                  , map ("-I" ++) (includeDirs bi ++ concatMap Installed.includeDirs deps)
+                  , map ("-L" ++) (extraLibDirs bi ++ concatMap Installed.libraryDirs deps)
                   , map ("-l" ++) (extraLibs bi)
                   ]
 
