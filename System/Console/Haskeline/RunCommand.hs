db2baabdab217128b4b76916a0397563a1f88b8e
Judah Jacobson <judah.jacobson@gmail.com>
2010-05-26 01:49:36 +0000

    Refactor RunCommand; also, if there's a sequence of keys that only consists of LineChanges, only draw the first and the last.

--- System/Console/Haskeline/RunCommand.hs
+++ System/Console/Haskeline/RunCommand.hs
@@ -20,31 +20,37 @@ runCommandLoop' :: forall t m a . (MonadTrans t, Term (t m), CommandMonad (t m),
 runCommandLoop' tops prefix cmds getEvent = do
     let s = lineChars prefix emptyIM
     drawLine s
-    loopKeys [] s (fmap ($ emptyIM) cmds)
-  where 
-    loopKeys :: [Key] -> LineChars -> KeyMap (CmdM m a) -> t m a
-    loopKeys [] s processor = do -- no keys left, so read some more
+    readMoreKeys s (fmap ($ emptyIM) cmds)
+  where
+    readMoreKeys :: LineChars -> KeyMap (CmdM m a) -> t m a
+    readMoreKeys s next = do
         event <- handle (\(e::SomeException) -> moveToNextLine s
                                     >> throwIO e) getEvent
         case event of
                     ErrorEvent e -> moveToNextLine s >> throwIO e
                     WindowResize -> drawReposition tops s
-                                    >> loopKeys [] s processor
+                                        >> readMoreKeys s next
                     KeyInput ks -> do
                         bound_ks <- mapM (lift . asks . lookupKeyBinding) ks
-                        loopKeys (concat bound_ks) s processor
-    loopKeys (k:ks) s processor = case lookupKM processor k of
-                        Nothing -> actBell >> loopKeys [] s processor
-                        Just (Consumed cmd) -> loopCmd ks s cmd
-                        Just (NotConsumed cmd) -> loopCmd (k:ks) s cmd
+                        loopCmd s $ applyKeysToMap (concat bound_ks) next
 
-    loopCmd :: [Key] -> LineChars -> CmdM m a -> t m a
-    loopCmd ks s (GetKey next) = loopKeys ks s next
-    loopCmd ks s (DoEffect e next) = do
-                                        t <- drawEffect prefix s e
-                                        loopCmd ks t next
-    loopCmd ks s (CmdM next) = lift next >>= loopCmd ks s
-    loopCmd _ s (Result x) = moveToNextLine s >> return x
+    loopCmd :: LineChars -> CmdM m a -> t m a
+    loopCmd s (GetKey next) = readMoreKeys s next
+    loopCmd s (DoEffect (LineChange f) next) = elideChanges s (f prefix) next
+    loopCmd s (DoEffect e next) = do
+                                    t <- drawEffect prefix s e
+                                    loopCmd t next
+    loopCmd s (CmdM next) = lift next >>= loopCmd s
+    loopCmd s (Result x) = moveToNextLine s >> return x
+
+    -- If there are multiple consecutive LineChanges, only render the diff
+    -- between the first and the last.  This greatly improves speed when
+    -- a large amount of text is pasted in at once.
+    elideChanges :: LineChars -> LineChars -> CmdM m a -> t m a
+    elideChanges s _ (DoEffect (LineChange f) next)
+                                    = elideChanges s (f prefix) next
+    elideChanges s t next = drawLineDiff s t >> loopCmd t next
+
 
 drawEffect :: (MonadTrans t, Term (t m), MonadReader Prefs m)
     => String -> LineChars -> Effect -> t m LineChars
@@ -82,3 +88,20 @@ drawReposition tops s = do
         reposition oldLayout s
 
 
+---------------
+-- Traverse through the tree of keybindings, using the given keys.
+-- Remove as many GetKeys as possible.
+applyKeysToMap :: Monad m => [Key] -> KeyMap (CmdM m a)
+                                -> CmdM m a
+applyKeysToMap [] next = GetKey next
+applyKeysToMap (k:ks) next = case lookupKM next k of
+    Nothing -> DoEffect RingBell $ GetKey next
+    Just (Consumed cmd) -> applyKeysToCmd ks cmd
+    Just (NotConsumed cmd) -> applyKeysToCmd (k:ks) cmd
+
+applyKeysToCmd :: Monad m => [Key] -> CmdM m a
+                                -> CmdM m a
+applyKeysToCmd ks (GetKey next) = applyKeysToMap ks next
+applyKeysToCmd ks (DoEffect e next) = DoEffect e (applyKeysToCmd ks next)
+applyKeysToCmd ks (CmdM next) = CmdM $ liftM (applyKeysToCmd ks) next
+applyKeysToCmd _ (Result x) = Result x
