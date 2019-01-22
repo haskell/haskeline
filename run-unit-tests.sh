952346bc0f77ec1ad2e9024b9f7c4d82c3f196f6
Kamil Dworakowski <kamil@dworakowski.name>
2019-01-22 13:44:16 +0100

    prefix executables with 'haskeline-' and run the tests inside tests dir

--- run-unit-tests.sh
+++ run-unit-tests.sh
@@ -1,1 +1,2 @@
-stack exec tests $(stack path --local-install-root)/bin/examples-Test
+cd tests # one of the tab completion tests is completing the dummy- dir in tests
+stack exec haskeline-tests $(stack path --local-install-root)/bin/haskeline-examples-Test
